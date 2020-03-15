5f0fc1d745e44aa3c666346960bd8e32bf3936e3
Waldemar Florkiewicz <waldziu3@gmail.com>
2020-03-15 13:21:09 +0000

    changes

--- src/main/java/ComparableAndComparator/Main.java
+++ src/main/java/ComparableAndComparator/Main.java
@@ -8,20 +8,16 @@ public class Main {
 
        Integer[] array = {1,2,3,4,5,6,7,8,9,11,10,12,20,14,15,16,17,18,19,13};
         Arrays.sort(array);
-        for (int a : array){
-            System.out.print(a + " ");
-        }
+        System.out.print(Arrays.toString(array));
         System.out.println();
 
         Arrays.sort(array, new Comparator<Integer>() {
             @Override
-            public int compare(Integer integer, Integer t1) {
-                return -integer.compareTo(t1);
+            public int compare(Integer t1, Integer t2) {
+                return -Integer.compare(t1,t2);
             }
         });
 
-        for (Integer b : array){
-            System.out.print(b + " ");
-        }
+        System.out.print(Arrays.toString(array));
     }
 }
