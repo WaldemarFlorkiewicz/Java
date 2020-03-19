dab06873622cc67680983c07df70b7cebbfade63
Waldemar Florkiewicz <waldziu3@gmail.com>
2020-03-19 18:53:51 +0000

    println changes

--- src/main/java/Maps/Company.java
+++ src/main/java/Maps/Company.java
@@ -7,18 +7,18 @@ public class Company {
     static HashMap<String, Employee> companyMap = new HashMap<>();
     Scanner scanner = new Scanner(System.in);
 
-    public  void addEmpplyee() {
-            System.out.println("Put name of employee: ");
-            String name = scanner.nextLine().toLowerCase();
-            System.out.println("Put surname of employee:");
-            String surname = scanner.nextLine().toLowerCase();
-            System.out.println("Put salary of employee: ");
-            double salary = scanner.nextDouble();
-            scanner.nextLine();
-            Employee employee = new Employee(name, surname, salary);
-            StringBuilder stringBuilder = new StringBuilder();
-            String fullKey = String.valueOf(stringBuilder.append(name + surname)).toLowerCase();
-            companyMap.put(fullKey, employee);
+    public void addEmpplyee() {
+        System.out.println("Put name of employee: ");
+        String name = scanner.nextLine().toLowerCase();
+        System.out.println("Put surname of employee:");
+        String surname = scanner.nextLine().toLowerCase();
+        System.out.println("Put salary of employee: ");
+        double salary = scanner.nextDouble();
+        scanner.nextLine();
+        Employee employee = new Employee(name, surname, salary);
+        StringBuilder stringBuilder = new StringBuilder();
+        String fullKey = String.valueOf(stringBuilder.append(name + surname)).toLowerCase();
+        companyMap.put(fullKey, employee);
 
     }
 
@@ -32,8 +32,8 @@ public class Company {
 
     }
 
-    public void getSizeOfMap(){
-        System.out.println(companyMap.size());
+    public void getSizeOfMap() {
+        System.out.println("Company employess: " + companyMap.size());
     }
 
 
