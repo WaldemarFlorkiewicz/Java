b20d6ba5537c2a60ea996f2aaaaaa64b2f867593
Waldemar Florkiewicz <waldziu3@gmail.com>
2020-04-05 14:13:01 +0100

    Optional

---
+++ src/main/java/Optional/ContactReader.java
@@ -0,0 +1,27 @@
+package Optional;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Optional;
+import java.util.Scanner;
+import java.util.function.Consumer;
+
+class ContactReader {
+    static Optional<ContactManager> readFile(String fileName) {
+        try (Scanner scanner = new Scanner(new File(fileName))) {
+            ContactManager contactManager = new ContactManager();
+            while (scanner.hasNextLine()) {
+                Contact contact = createContactFromText(scanner.nextLine());
+                contactManager.addContact(contact);
+            }
+            return Optional.of(contactManager);
+        } catch (FileNotFoundException e) {
+            return Optional.empty();
+        }
+    }
+
+    private static Contact createContactFromText(String text) {
+        String[] data = text.split(";");
+        return new Contact(data[0], data[1], data[2], data[3]);
+    }
+}
